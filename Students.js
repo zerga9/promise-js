292b19311949710d3573d511665f30823607668a
Gadiza Zerari <gadizaz@hotmail.com>
2018-08-01 08:43:57 +0100

    Install Mocha and function is returning promise

---
+++ Students.js
@@ -0,0 +1,46 @@
+function studentDataService(cohort, minimumAge, gender) {
+  const students = {
+    august: [
+      { name: "Andy", age: 29, gender: "male" },
+
+      { name: "Briony", age: 32, gender: "female" },
+
+      { name: "Catherine", age: 19, gender: "female" },
+
+      { name: "David", age: 22, gender: "male" },
+
+      { name: "Eleanor", age: 20, gender: "female" },
+
+      { name: "Freddie", age: 19, gender: "male" },
+
+      { name: "Gertrude", age: 28, gender: "female" }
+    ],
+
+    september: [
+      { name: "Ingrid", age: 42, gender: "female" },
+
+      { name: "James", age: 19, gender: "male" },
+
+      { name: "Keith", age: 26, gender: "male" },
+
+      { name: "Lavender", age: 27, gender: "female" },
+
+      { name: "Mary", age: 28, gender: "female" },
+
+      { name: "Norman", age: 22, gender: "male" },
+
+      { name: "Oliver", age: 26, gender: "male" }
+    ]
+  };
+  return new Promise((resolve, reject) => {
+    resolve(students[cohort]);
+  });
+}
+
+class StudentFilter {
+  getStudents(cohort, mininumAge, gender) {
+    return new Promise((resolve, reject) => {
+      resolve(studentDataService());
+    });
+  }
+}
---
+++ test.js
@@ -0,0 +1,16 @@
+var expect = require("chai").expect;
+
+describe("NewsController", function() {
+  var students;
+
+  // beforeEach(function() {
+  //   students = new getStudents();
+  // });
+
+  it("assertion success", async () => {
+    students = new StudentFilter();
+    student.getStudents();
+    const result = await resolvingPromise;
+    expect(result).to.equal("promise resolved");
+  });
+});
