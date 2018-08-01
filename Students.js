a580616c9f1e9c5e4744591ea1aec6ec78c600c0
Gadiza Zerari <gadizaz@hotmail.com>
2018-08-01 09:11:17 +0100

    add helper function in studentfilter to get all students in array

--- Students.js
+++ Students.js
@@ -41,6 +41,25 @@ function studentDataService(cohort, minimumAge, gender) {
 }
 
 class StudentFilter {
+  // helper function to  merge all student arrays
+  // expects students to be an Object {key: [value <array>]}
+
+  //   const object1 = {
+  //   a: 'somestring',
+  //   b: 42,
+  //   c: false
+  // };
+  //
+  // console.log(Object.values(object1));
+  // expected output: Array ["somestring", 42, false]
+
+  getAllStudents(students) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+
+    return Object.values(students).reduce((accumulator, current) => {
+      return [...accumulator, ...current];
+    }, []);
+  }
   getStudents(cohort, mininumAge, gender) {
     // method returns a promise
     return new Promise((resolve, reject) => {
